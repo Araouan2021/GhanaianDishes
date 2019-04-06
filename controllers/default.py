25f1284140eb2b3b4954291002711fdef5c15298
Bola Bally <bola@Bolas-MacBook-Pro.local>
2019-04-06 09:49:12 +0000

    addsearch.html

--- controllers/default.py
+++ controllers/default.py
@@ -21,3 +21,8 @@ def delete():
     else:
         return 'No Recipe With the ID found'
 
+def search():
+    query = db.recipes.title.contains(request.vars.search)
+    recipes = db(query).select(orderby=db.recipes.title)
+    return dict(recipes=recipes)
+
