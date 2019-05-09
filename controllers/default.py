197c0ac156a5cb49f999fd99b5276d537551abd2
Bola Bally <bolabally@gmail.com>
2019-05-09 14:12:16 +0000

    addshow.html

--- controllers/default.py
+++ controllers/default.py
@@ -56,17 +56,11 @@ def manage():
     grid = SQLFORM.smartgrid(db.recipes, linked_tables=['recipes'])
     return dict(grid=grid)
 
-#@auth.requires_login()
-def postReview():
-    recipes = db.recipes(request.args(0, cast=int)) or redirect(URL('postReview'))
-    db.review.recipes_id.default = recipes.id
-    form = SQLFORM(db.review)
-    if form.process().accepted:
-        response.flash = 'your review has been posted'
-    reviews = db(db.review.recipes_id == recipes.id).select(orderby=db.review.id)
-    return dict(recipes=recipes, reviews=reviews, form=form)
+def show():
+    recipes = db.recipes(request.args(0, cast=int)) or redirect(URL('index'))
+    return dict(recipes=recipes)
 
-      
+
     
         
 
--- models/db.py
+++ models/db.py
@@ -5,18 +5,19 @@ auth = Auth(db)
 auth.define_tables(username=False)
 
 db.define_table('recipes',
-                Field('title'),
+                Field('title', unique=True),
                 Field('ingredients', 'text'),
                 Field('method', 'text'),
                 Field('size'),
                 Field('time'),
                 Field('file', 'upload'),
-                auth.signature              
-                )
-
+                format = '%(title)s')
+                
 db.define_table('review',
                 Field('recipes_id', 'reference recipes'),
                 Field('author'),
                 Field('email'),
                 Field('body', 'text'))
+                
+
 
